655f1e64b98ffb099aeb9705373e88fe4972bfbe
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2020-01-12 21:34:14 -0500

    HW 2

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Bryan Boyd',
+        email: 'bryan.boyd@sitechhs.com',
+        status: 'Happy'
+    },
+    {
+        id: 2,
+        name: 'Brian Jiang',
+        email: 'brian.jiang@sitechhs.com',
+        status: 'Depressed'
+    },
+    {
+        id: 3,
+        name: 'Michael Whalen',
+        email: 'michael.whalen@sitechhs.com',
+        status: 'Epic'
+    }
+];
+
+module.exports = members;
